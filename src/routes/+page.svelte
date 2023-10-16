86a793c55668e3b54fbe8f837a728265370273fd
c8e4 <c8e4@proton.me>
2023-10-16 18:16:17 +0200

    init blockly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Blockly from "blockly";
+    import { onMount } from "svelte";
+
+    const toolbox = {
+        kind: "flyoutToolbox",
+        contents: [
+            {
+                kind: "block",
+                type: "controls_repeat_ext",
+                inputs: {
+                    TIMES: {
+                        shadow: {
+                            type: "math_number",
+                            fields: {
+                                NUM: 5,
+                            },
+                        },
+                    },
+                },
+            },
+        ],
+    };
+
+    onMount(() => {
+        Blockly.inject("blocklyDiv", {
+            toolbox: toolbox,
+            scrollbars: false,
+            horizontalLayout: true,
+            toolboxPosition: "end",
+        });
+    });
+</script>
+
+<div id="blocklyDiv" />
+
+<style>
+    :global(html) {
+        min-height: 100vh;
+    }
+    :global(body) {
+        min-height: 100vh;
+        background: red;
+    }
+    #blocklyDiv {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+    }
+</style>
